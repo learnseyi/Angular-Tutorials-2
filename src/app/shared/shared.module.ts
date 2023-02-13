ba92e201587ded8f0f5f4ccbeecef333ba42395a
Seyi Agboola <learnseyi02@gmail.com>
2023-02-13 00:53:17 -0600

    created auth and modal services

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms'
 import { ModalComponent } from './modal/modal.component';
 
 
@@ -9,7 +10,8 @@ import { ModalComponent } from './modal/modal.component';
     ModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports:[ModalComponent]
 })
